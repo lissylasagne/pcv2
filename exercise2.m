a9630cd26e1784e4afb5b3766e71a7d85aad230e
Lissy <elisabeth.johanna.scholz@uni-weimar.de>
2019-11-18 17:52:37 +0100

    error

--- exercise2.m
+++ exercise2.m
@@ -1,20 +1,20 @@
 function exercise2()
-  I1 = imread("/home/lissy/Documents/ComputerVision/image1");
-  I2 = imread("/home/lissy/Documents/ComputerVision/image2");
-  I3 = imread("/home/lissy/Documents/ComputerVision/image3");
+  I1 = imread("/home/lissy/Documents/ComputerVision/pcv2/image1");
+  I2 = imread("/home/lissy/Documents/ComputerVision/pcv2/image2");
+  I3 = imread("/home/lissy/Documents/ComputerVision/pcv2/image3");
   
   figure(1);
   imshow(I1);
   [x1, y1] = ginput()
   disp("Pleade press ENTER when you're finished")
   
-  length = ndims(x1);
+  length = rows(x1);
   
   figure(2);
   imshow(I2);
-  [x2, y2] = ginput(length+1)
+  [x2, y2] = ginput(length)
   
   figure(3);
   imshow(I3);
-  [x3, y3] = ginput(length+1)
+  [x3, y3] = ginput(length)
   
